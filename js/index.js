3b890ebdff7ef945e3bb02bc25df71285ada9bb4
graphic <graphic@WFD306-17m.local>
2019-10-30 13:55:14 -0400

    v2

---
+++ js/index.js
@@ -0,0 +1,29 @@
+//string is a serie of characters
+console.log(`hello world`);
+console.log(`1`+`2`);
+//Number is a number
+console.log(123);
+console.log(3 + 5); //add
+console.log(3 - 5); // subtract
+console.log(3 * 5); //multiply
+console.log(3 / 5); //divide
+console.log(3 % 5); //modulus
+console.log(3 ** 5); //power/exponent
+
+let bgcolor = `grey`; // same as in css like --bgcolor: grey; color: var(--bgcolor);
+
+//Declaring variables, giving them starting values
+let customerName = `George`; //class name $ -
+let bitCoin = 0;
+//Hi george, your balance is: 0
+console.log (`Hi ${customerName}, Your balance is: 💰 ${bitCoin}`);
+
+//assigning a new value to a variable
+
+bitCoin = bitCoin + 4; 
+console.log (`Hi ${customerName}, Your balance is: 💰 ${bitCoin}`);
+//Hi george, your balance is: 4
+
+bitCoin = bitCoin - 1;
+console.log (`Hi ${customerName}, Your balance is: 💰 ${bitCoin}`);
+//Hi george, your balance is: 3
